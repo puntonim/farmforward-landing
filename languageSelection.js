9cc5a44f1d43ad26e01da824aae12c056be3996c
Sunilkumar Gavara <sunilkumargavara@gmail.com>
2020-06-19 17:44:45 +0530

    Localisation is done

---
+++ languageSelection.js
@@ -0,0 +1,141 @@
+const data = {
+  mainTitleEn:
+    "Grow better and more profitable potatoes with the farmforward app",
+  mainTitleHi: "फार्मफॉरवर्ड ऐप के साथ बेहतर और अधिक लाभदायक आलू उगाएं",
+  downloadButtonEn: "Free download for Android",
+  downloadButtonHi: "Android के लिए मुफ्त डाउनलोड",
+  turnYourPhoneEn: "Turn your phone into an agronomist",
+  turnYourPhoneHi: "अपने फोन को एग्रोनोमिस्ट में बदल दें",
+  turnYourPhoneDescEn:
+    "Receive weekly farm activities from Yara agronomists based on your planting date, from pre-planting to harvest.",
+  turnYourPhoneDescHi:
+    "पूर्व रोपण से लेकर कटाई तक आपकी रोपण तिथि के आधार पर यारा कृषिविदों की साप्ताहिक कृषि गतिविधियाँ प्राप्त करें।",
+  makeYourFieldsProfitableEn: "Make yields more profitable",
+  makeYourFieldsProfitableHi: "पैदावार को अधिक लाभदायक बनाते हैं",
+  makeYourFieldsProfitableDescEn:
+    "Play fun quizzes and read articles that help you improve your potato quality, reap higher yields, and learn new farm practises.",
+  makeYourFieldsProfitableDescHi:
+    "मज़ेदार क्विज़ खेलें और उन लेखों को पढ़ें जो आपको अपने आलू की गुणवत्ता में सुधार करने, उच्च पैदावार लेने और नए खेत प्रथाओं को सीखने में मदद करते हैं।",
+  becomeStarEn: "Become a Yara star farmer",
+  becomeStarHi: "यारा स्टार किसान बनें",
+  becomeStarDescEn:
+    "Complete activities to gain points and get to the top of the leaderboard. Be recognised as a star farmer in your community.",
+  becomeStarDescHi:
+    "अंक हासिल करने और लीडरबोर्ड के शीर्ष पर पहुंचने के लिए पूरी गतिविधियाँ। अपने समुदाय में एक स्टार किसान के रूप में पहचाना जाना चाहिए।",
+  howItWorksEn: "How it works",
+  howItWorksHi: "यह काम किस प्रकार करता है",
+  onYourFarmEn: "On your farm",
+  onYourFarmHi: "अपने खेत पर",
+  onYourFarmDescEn:
+    "Add your location, farm details, crop details, and planting date to set up your profile.",
+  onYourFarmDescHi:
+    "अपना प्रोफ़ाइल सेट करने के लिए अपना स्थान, खेत का विवरण, फसल का विवरण और रोपण तिथि जोड़ें।",
+  everyWeekEn: "Every week",
+  everyWeekHi: "प्रति सप्ताह",
+  everyWeekDescEn:
+    "Receive a notification when new farm activities are available each week. Record whether you did or did not do them and write notes.",
+  everyWeekDescHi:
+    "प्रत्येक सप्ताह नई कृषि गतिविधियाँ उपलब्ध होने पर एक सूचना प्राप्त करें। रिकॉर्ड करें कि आपने उन्हें किया या नहीं और नोट्स लिखें।",
+  throughOutYourSeasonEn: "Throughout your season",
+  throughOutYourSeasonHi: "अपने पूरे मौसम में",
+  throughOutYourSeasonDescEn:
+    "Play fun quizzes and read useful articles that improve your potato-growing knowledge.",
+  throughOutYourSeasonDescHi:
+    "मजेदार क्विज़ खेलें और अपने आलू उगाने वाले ज्ञान को बेहतर बनाने वाले उपयोगी लेख पढ़ें।",
+  whatAgronomistSayEn: "What agronomists say",
+  whatAgronomistSayHi: "कृषि विज्ञानी क्या कहते हैं",
+  whatAgronomistSayDescEn:
+    "The farmforward app is specialised for processed potato farmers in India. It is available in Hindi and English, and is supported by Yara's 100+ years of crop knowledge.",
+  whatAgronomistSayDescHi:
+    "फार्मफोवर्ड ऐप भारत में संसाधित आलू किसानों के लिए विशिष्ट है। यह हिंदी और अंग्रेजी में उपलब्ध है, और यारा के 100+ वर्षों के फसल ज्ञान द्वारा समर्थित है।",
+  neerajKumarEn: "Neeraj Kumar",
+  neerajKumarHi: "नीरज कुमार",
+  neerajKumarDesgEn: "Senior Digital Potato Agronomist, India",
+  neerajKumarDesgHi: "वरिष्ठ डिजिटल आलू एग्रोनोमिस्ट, भारत",
+  neerajKumarDescEn:
+    '"Yara farmforward helps potato farmers learn by recommending farm activities throughout the season that improve crop management, production, and value."',
+  neerajKumarDescHi:
+    '"यारा फार्मफ़ॉर्मवर्ड आलू के किसानों को फसल प्रबंधन, उत्पादन और मूल्य में सुधार करने वाले पूरे मौसम में कृषि गतिविधियों की सिफारिश करके सीखने में मदद करता है।"',
+  swathiNayakEn: "Swati Nayak",
+  swathiNayakHi: "स्वाति नायक",
+  swathiNayakDesgEn: "Yara Digital Agronomist, India",
+  swathiNayakDesgHi: "यारा डिजिटल एग्रोनॉमिस्ट, भारत",
+  swathiNayakDescEn:
+    '"The farmforward app is designed specifically for smallholder potato farmers. It provides them with important insights and useful solutions customised to their needs."',
+  swathiNayakDescHi:
+    '"फार्मफोरवर्ड ऐप को विशेष रूप से छोटे धारक आलू किसानों के लिए डिज़ाइन किया गया है। यह उन्हें महत्वपूर्ण अंतर्दृष्टि और उनकी आवश्यकताओं के अनुरूप उपयोगी समाधान प्रदान करता है।"',
+  downloadTodayEn:
+    "Download today to start receiving your personalised potato-growing activities.",
+  downloadTodayHi:
+    "अपने व्यक्तिगत आलू उगाने वाली गतिविधियों को प्राप्त करने के लिए आज ही डाउनलोड करें।",
+  copyRightEn:
+    "Copyright © 2020 Yara. All Rights Reserved. This website uses cookies to give you the best, most relevant experience. By using this website you give us consent to store your cookie as per our Privacy Policy.",
+  copyRightHi:
+    "कॉपीराइट © 2020 यारा। सभी अधिकार सुरक्षित। यह वेबसाइट आपको सबसे अच्छा, सबसे अधिक प्रासंगिक अनुभव देने के लिए कुकीज़ का उपयोग करती है। इस वेबसाइट का उपयोग करके आप हमारी गोपनीयता नीति के अनुसार अपनी कुकी को स्टोर करने के लिए हमें सहमति देते हैं।",
+  privacyPolicyEn: "Privacy Policy",
+  privacyPolicyHi: "गोपनीयता नीति",
+  termsOfUseEn: "Terms of use",
+  termsOfUseHi: "उपयोग की शर्तें",
+};
+
+function languageSelection(language) {
+  document.getElementById("mainTitle").innerHTML = data[`mainTitle${language}`];
+  document.getElementById("downloadButtonTop").innerHTML =
+    data[`downloadButton${language}`];
+  document.getElementById("turnYourPhoneTitle").innerHTML =
+    data[`turnYourPhone${language}`];
+  document.getElementById("turnYourPhoneDesc").innerHTML =
+    data[`turnYourPhoneDesc${language}`];
+  document.getElementById("makeYieldTitle").innerHTML =
+    data[`makeYourFieldsProfitable${language}`];
+  document.getElementById("makeYieldDesc").innerHTML =
+    data[`makeYourFieldsProfitableDesc${language}`];
+  document.getElementById("becomeStarTitle").innerHTML =
+    data[`becomeStar${language}`];
+  document.getElementById("becomeStarDesc").innerHTML =
+    data[`becomeStarDesc${language}`];
+  document.getElementById("howItWorks").innerHTML =
+    data[`howItWorks${language}`];
+  document.getElementById("onYourFarmTitle").innerHTML =
+    data[`onYourFarm${language}`];
+  document.getElementById("onYourFarmDesc").innerHTML =
+    data[`onYourFarmDesc${language}`];
+  document.getElementById("everyWeekTitle").innerHTML =
+    data[`everyWeek${language}`];
+  document.getElementById("everyWeekDesc").innerHTML =
+    data[`everyWeekDesc${language}`];
+  document.getElementById("throughoutSeasonTitle").innerHTML =
+    data[`throughOutYourSeason${language}`];
+  document.getElementById("throughoutSeasonDesc").innerHTML =
+    data[`throughOutYourSeasonDesc${language}`];
+  document.getElementById("whatAgronomistSayTitle").innerHTML =
+    data[`whatAgronomistSay${language}`];
+  document.getElementById("whatAgronomistSayDesc").innerHTML =
+    data[`whatAgronomistSayDesc${language}`];
+  document.getElementById("neerajTitle").innerHTML =
+    data[`neerajKumar${language}`];
+  document.getElementById("neerajDesg").innerHTML =
+    data[`neerajKumarDesg${language}`];
+  document.getElementById("neerajDesc").innerHTML =
+    data[`neerajKumarDesc${language}`];
+  document.getElementById("swathiTitle").innerHTML =
+    data[`swathiNayak${language}`];
+  document.getElementById("swathiDesg").innerHTML =
+    data[`swathiNayakDesg${language}`];
+  document.getElementById("swathiDesc").innerHTML =
+    data[`swathiNayakDesc${language}`];
+  document.getElementById("downloadDesc").innerHTML =
+    data[`downloadToday${language}`];
+  document.getElementById("downloadButtonBottom").innerHTML =
+    data[`downloadButton${language}`];
+  document.getElementById("privacyPolicy").innerHTML =
+    data[`privacyPolicy${language}`];
+  document.getElementById("termsOfUse").innerHTML =
+    data[`termsOfUse${language}`];
+  document.getElementById("copyRight").innerHTML = data[`copyRight${language}`];
+  const enButton = document.getElementById("enButton");
+  const hiButton = document.getElementById("hiButton");
+  const isEnglishSelected = language == "En";
+  enButton.style.color = isEnglishSelected ? "#7a563d" : "#0f0f0f";
+  hiButton.style.color = isEnglishSelected ? "#0f0f0f" : "#7a563d";
+}
