45375f779e95fdbb356ffebdc80fc250d4caf982
puntonim <puntonim@gmail.com>
2020-06-30 12:04:50 +0800

    Copy files from the other repo

---
+++ languageSelection.js
@@ -0,0 +1,267 @@
+const data = {
+  mainTitleEn:
+    "Grow better and more profitable potatoes with the farmforward app",
+  mainTitleHi: "farmforward ऐप के साथ बेहतर और अधिक लाभदायक आलू उगाएं",
+  downloadButtonEn: "Free download for Android",
+  downloadButtonHi: "एंड्रॉयड के लिए मुफ्त डाउनलोड",
+  turnYourPhoneEn: "Turn your phone into an agronomist",
+  turnYourPhoneHi: "अपने फोन को एग्रोनोमिस्ट में बदल दें",
+  turnYourPhoneDescEn:
+    "Receive weekly farm activities from Yara agronomists based on your planting date, from pre-planting to harvest.",
+  turnYourPhoneDescHi:
+    "बिजाई के पहले से लेकर कटाई तक आपकी बुवाई की तारीख के आधार पर Yara कृषिविदों की साप्ताहिक कृषि गतिविधियाँ प्राप्त करें।",
+  makeYourFieldsProfitableEn: "Make yields more profitable",
+  makeYourFieldsProfitableHi: "पैदावार को अधिक लाभदायक बनाएं",
+  makeYourFieldsProfitableDescEn:
+    "Play fun quizzes and read articles that help you improve your potato quality, reap higher yields, and learn new farm practises.",
+  makeYourFieldsProfitableDescHi:
+    "मजेदार क्विज़ खेलें और उन लेखों को पढ़ें जो आपको अपने आलू की गुणवत्ता में सुधार करने, उच्च पैदावार लेने और नए खेत अभ्यास सीखने में मदद करते हैं।",
+  becomeStarEn: "Become a Yara star farmer",
+  becomeStarHi: "एक Yara स्टार किसान बनें",
+  becomeStarDescEn:
+    "Complete activities to gain points and get to the top of the leaderboard. Be recognised as a star farmer in your community.",
+  becomeStarDescHi:
+    "अंक हासिल करने और लीडरबोर्ड के शीर्ष पर पहुंचने के लिए गतिविधियाँ पूर्ण करें। अपनी कम्युनीटी में एक स्टार किसान के रूप में पहचान बनाएं।",
+  howItWorksEn: "How it works",
+  howItWorksHi: "इसके काम करने का तरीका",
+  onYourFarmEn: "On your farm",
+  onYourFarmHi: "अपने खेत पर",
+  onYourFarmDescEn:
+    "Add your location, farm details, crop details, and planting date to set up your profile.",
+  onYourFarmDescHi:
+    "अपना प्रोफ़ाइल सेट करने के लिए अपना स्थान, खेत का विवरण, फसल का विवरण और रोपण तिथि जोड़ें।",
+  everyWeekEn: "Every week",
+  everyWeekHi: "प्रति सप्ताह",
+  everyWeekDescEn:
+    "Receive a notification when new farm activities are available each week. Record whether you did or did not do them and write notes.",
+  everyWeekDescHi:
+    "प्रत्येक सप्ताह नई कृषि गतिविधियाँ उपलब्ध होने पर एक सूचना प्राप्त करें। रिकॉर्ड करें कि आपने उन्हें किया या नहीं और नोट्स लिखें।",
+  throughOutYourSeasonEn: "Throughout your season",
+  throughOutYourSeasonHi: "अपने पूरे मौसम के दौरान",
+  throughOutYourSeasonDescEn:
+    "Play fun quizzes and read useful articles that improve your potato-growing knowledge.",
+  throughOutYourSeasonDescHi:
+    "मजेदार क्विज़ खेलें और अपने आलू उगाने वाले ज्ञान को बेहतर बनाने वाले उपयोगी लेख पढ़ें।",
+  whatAgronomistSayEn: "What agronomists say",
+  whatAgronomistSayHi: "कृषि विज्ञानी क्या कहते हैं",
+  whatAgronomistSayDescEn:
+    "The farmforward app is specialised for processed potato farmers in India. It is available in Hindi and English, and is supported by Yara's 100+ years of crop knowledge.",
+  whatAgronomistSayDescHi:
+    "farmforward ऐप भारत में प्रोसेस्ड आलू किसानों के लिए विशेष है। यह हिंदी और अंग्रेजी में उपलब्ध है, और Yara के 100 + वर्षों के फसल ज्ञान द्वारा समर्थित है।",
+  neerajKumarEn: "Neeraj Kumar",
+  neerajKumarHi: "नीरज कुमार",
+  neerajKumarDesgEn: "Senior Digital Potato Agronomist, India",
+  neerajKumarDesgHi: "सीनियर डिजिटल पोटेटो एग्रोनोमिस्ट, भारत",
+  neerajKumarDescEn:
+    '"Yara farmforward helps potato farmers learn by recommending farm activities throughout the season that improve crop management, production, and value."',
+  neerajKumarDescHi:
+    '"Yara farmforward आलू के किसानों को फसल के प्रबंधन, उत्पादन और मूल्य में सुधार करने वाले पूरे मौसम में कृषि गतिविधियों की सिफारिश करने में मदद करता है।"',
+  swathiNayakEn: "Swati Nayak",
+  swathiNayakHi: "स्वाति नायक",
+  swathiNayakDesgEn: "Yara Digital Agronomist, India",
+  swathiNayakDesgHi: "Yara डिजिटल एग्रोनॉमिस्ट, भारत",
+  swathiNayakDescEn:
+    '"The farmforward app is designed specifically for smallholder potato farmers. It provides them with important insights and useful solutions customised to their needs."',
+  swathiNayakDescHi:
+    '"farmforward ऐप विशेष रूप से आलू उगानेवाले छोटे किसानों के लिए तैयार किया गया है। यह उन्हें महत्वपूर्ण अंतर्दृष्टि और उनकी आवश्यकताओं के लिए उपयोगी समाधान प्रदान करता है।"',
+  downloadTodayEn:
+    "Download today to start receiving your personalised potato-growing activities.",
+  downloadTodayHi:
+    "अपने व्यक्तिगत आलू उगाने की गतिविधियों को प्राप्त करना शुरू करने के लिए आज डाउनलोड करें।",
+  copyRightEn:
+    "Copyright © 2020 Yara. All Rights Reserved. This website uses cookies to give you the best, most relevant experience. By using this website you give us consent to store your cookie as per our Privacy Policy.",
+  copyRightHi:
+    "कॉपीराइट © 2020 Yara. सभी अधिकार सुरक्षित। यह वेबसाइट आपको सबसे अच्छा, सबसे प्रासंगिक अनुभव देने के लिए कुकीज़ का उपयोग करती है। इस वेबसाइट का उपयोग करके आप हमें हमारी गोपनीयता नीति के अनुसार अपनी कुकीज़ स्टोर करने के लिए सहमति देते हैं।",
+  privacyPolicyEn: "Privacy Policy",
+  privacyPolicyHi: "गोपनीयता नीति",
+  termsOfUseEn: "Terms of use",
+  termsOfUseHi: "उपयोग की शर्तें",
+};
+
+function languageSelection() {
+  const href = window.location.href;
+  const language = href.includes("lang=en") ? "En" : "Hi";
+  const isLanguageEnglish = language === "En";
+  const mainTitle = document.getElementById("mainTitle");
+  mainTitle.style.fontFamily = isLanguageEnglish
+    ? "Max-Regular"
+    : "Noto Sans Devanagari UI";
+  mainTitle.innerHTML = data[`mainTitle${language}`];
+
+  const downloadButtonTop = document.getElementById("downloadButtonTop");
+  downloadButtonTop.style.fontFamily = isLanguageEnglish
+    ? "Max-Regular"
+    : "Noto Sans Devanagari UI Bold";
+  downloadButtonTop.innerHTML = data[`downloadButton${language}`];
+
+  const turnYourPhoneTitle = document.getElementById("turnYourPhoneTitle");
+  turnYourPhoneTitle.style.fontFamily = isLanguageEnglish
+    ? "Max-Regular"
+    : "Noto Sans Devanagari UI";
+  turnYourPhoneTitle.innerHTML = data[`turnYourPhone${language}`];
+
+  const turnYourPhoneDesc = document.getElementById("turnYourPhoneDesc");
+  turnYourPhoneDesc.style.fontFamily = isLanguageEnglish
+    ? "Noto Sans"
+    : "Noto Sans Devanagari UI";
+  turnYourPhoneDesc.innerHTML = data[`turnYourPhoneDesc${language}`];
+
+  const makeYieldTitle = document.getElementById("makeYieldTitle");
+  makeYieldTitle.style.fontFamily = isLanguageEnglish
+    ? "Max-Regular"
+    : "Noto Sans Devanagari UI";
+  makeYieldTitle.innerHTML = data[`makeYourFieldsProfitable${language}`];
+
+  const makeYieldDesc = document.getElementById("makeYieldDesc");
+  makeYieldDesc.style.fontFamily = isLanguageEnglish
+    ? "Noto Sans"
+    : "Noto Sans Devanagari UI";
+  makeYieldDesc.innerHTML = data[`makeYourFieldsProfitableDesc${language}`];
+
+  const becomeStarTitle = document.getElementById("becomeStarTitle");
+  becomeStarTitle.style.fontFamily = isLanguageEnglish
+    ? "Max-Regular"
+    : "Noto Sans Devanagari UI";
+  becomeStarTitle.innerHTML = data[`becomeStar${language}`];
+
+  const becomeStarDesc = document.getElementById("becomeStarDesc");
+  becomeStarDesc.style.fontFamily = isLanguageEnglish
+    ? "Noto Sans"
+    : "Noto Sans Devanagari UI";
+  becomeStarDesc.innerHTML = data[`becomeStarDesc${language}`];
+
+  const howItWorks = document.getElementById("howItWorks");
+  howItWorks.style.fontFamily = isLanguageEnglish
+    ? "Max-Regular"
+    : "Noto Sans Devanagari UI";
+  howItWorks.innerHTML = data[`howItWorks${language}`];
+
+  const onYourFarmTitle = document.getElementById("onYourFarmTitle");
+  onYourFarmTitle.style.fontFamily = isLanguageEnglish
+    ? "Max-Regular"
+    : "Noto Sans Devanagari UI";
+  onYourFarmTitle.innerHTML = data[`onYourFarm${language}`];
+
+  const onYourFarmDesc = document.getElementById("onYourFarmDesc");
+  onYourFarmDesc.style.fontFamily = isLanguageEnglish
+    ? "Noto Sans"
+    : "Noto Sans Devanagari UI";
+  onYourFarmDesc.innerHTML = data[`onYourFarmDesc${language}`];
+
+  const everyWeekTitle = document.getElementById("everyWeekTitle");
+  everyWeekTitle.style.fontFamily = isLanguageEnglish
+    ? "Max-Regular"
+    : "Noto Sans Devanagari UI";
+  everyWeekTitle.innerHTML = data[`everyWeek${language}`];
+
+  const everyWeekDesc = document.getElementById("everyWeekDesc");
+  everyWeekDesc.style.fontFamily = isLanguageEnglish
+    ? "Noto Sans"
+    : "Noto Sans Devanagari UI";
+  everyWeekDesc.innerHTML = data[`everyWeekDesc${language}`];
+
+  const throughoutSeasonTitle = document.getElementById(
+    "throughoutSeasonTitle"
+  );
+  throughoutSeasonTitle.style.fontFamily = isLanguageEnglish
+    ? "Max-Regular"
+    : "Noto Sans Devanagari UI";
+  throughoutSeasonTitle.innerHTML = data[`throughOutYourSeason${language}`];
+
+  const throughoutSeasonDesc = document.getElementById("throughoutSeasonDesc");
+  throughoutSeasonDesc.style.fontFamily = isLanguageEnglish
+    ? "Noto Sans"
+    : "Noto Sans Devanagari UI";
+  throughoutSeasonDesc.innerHTML = data[`throughOutYourSeasonDesc${language}`];
+
+  const whatAgronomistSayTitle = document.getElementById(
+    "whatAgronomistSayTitle"
+  );
+  whatAgronomistSayTitle.style.fontFamily = isLanguageEnglish
+    ? "Max-Regular"
+    : "Noto Sans Devanagari UI";
+  whatAgronomistSayTitle.innerHTML = data[`whatAgronomistSay${language}`];
+
+  const whatAgronomistSayDesc = document.getElementById(
+    "whatAgronomistSayDesc"
+  );
+  whatAgronomistSayTitle.style.fontFamily = isLanguageEnglish
+    ? "Noto Sans"
+    : "Noto Sans Devanagari UI";
+  whatAgronomistSayDesc.innerHTML = data[`whatAgronomistSayDesc${language}`];
+
+  const neerajTitle = document.getElementById("neerajTitle");
+  neerajTitle.style.fontFamily = isLanguageEnglish
+    ? "Max-Regular"
+    : "Noto Sans Devanagari UI";
+  neerajTitle.innerHTML = data[`neerajKumar${language}`];
+
+  const neerajDesg = document.getElementById("neerajDesg");
+  neerajDesg.style.fontFamily = isLanguageEnglish
+    ? "Max-Regular"
+    : "Noto Sans Devanagari UI";
+  neerajDesg.innerHTML = data[`neerajKumarDesg${language}`];
+
+  const neerajDesc = document.getElementById("neerajDesc");
+  neerajDesc.style.fontFamily = isLanguageEnglish
+    ? "Noto Sans"
+    : "Noto Sans Devanagari UI";
+  neerajDesc.innerHTML = data[`neerajKumarDesc${language}`];
+
+  const swathiTitle = document.getElementById("swathiTitle");
+  swathiTitle.style.fontFamily = isLanguageEnglish
+    ? "Max-Regular"
+    : "Noto Sans Devanagari UI";
+  swathiTitle.innerHTML = data[`swathiNayak${language}`];
+
+  const swathiDesg = document.getElementById("swathiDesg");
+  swathiDesg.style.fontFamily = isLanguageEnglish
+    ? "Max-Regular"
+    : "Noto Sans Devanagari UI";
+  swathiDesg.innerHTML = data[`swathiNayakDesg${language}`];
+
+  const swathiDesc = document.getElementById("swathiDesc");
+  swathiDesc.style.fontFamily = isLanguageEnglish
+    ? "Noto Sans"
+    : "Noto Sans Devanagari UI";
+  swathiDesc.innerHTML = data[`swathiNayakDesc${language}`];
+
+  const downloadDesc = document.getElementById("downloadDesc");
+  downloadDesc.style.fontFamily = isLanguageEnglish
+    ? "Noto Sans"
+    : "Noto Sans Devanagari UI";
+  downloadDesc.innerHTML = data[`downloadToday${language}`];
+
+  const downloadButtonBottom = document.getElementById("downloadButtonBottom");
+  downloadButtonBottom.style.fontFamily = isLanguageEnglish
+    ? "Noto Sans"
+    : "Noto Sans Devanagari UI Bold";
+  downloadButtonBottom.innerHTML = data[`downloadButton${language}`];
+
+  const privacyPolicy = document.getElementById("privacyPolicy");
+  privacyPolicy.style.fontFamily = isLanguageEnglish
+    ? "Noto Sans"
+    : "Noto Sans Devanagari UI";
+  privacyPolicy.innerHTML = data[`privacyPolicy${language}`];
+
+  const termsOfUse = document.getElementById("termsOfUse");
+  termsOfUse.style.fontFamily = isLanguageEnglish
+    ? "Noto Sans"
+    : "Noto Sans Devanagari UI";
+  termsOfUse.innerHTML = data[`termsOfUse${language}`];
+
+  const copyRight = document.getElementById("copyRight");
+  copyRight.style.fontFamily = isLanguageEnglish
+    ? "Noto Sans"
+    : "Noto Sans Devanagari UI";
+  copyRight.innerHTML = data[`copyRight${language}`];
+
+  const enButton = document.getElementById("enButton");
+  const hiButton = document.getElementById("hiButton");
+  const isEnglishSelected = language === "En";
+
+  enButton.style.color = isEnglishSelected ? "#7a563d" : "#0f0f0f66";
+  hiButton.style.color = isEnglishSelected ? "#0f0f0f66" : "#7a563d";
+}
+
+languageSelection();
